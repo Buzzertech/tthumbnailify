686cf1a3d7721b13f414f55a9c53d35cf35e4850
Buzzertech <prasadnayak1006@gmail.com>
2018-05-26 20:07:31 +0530

    explicitly delete keys and components once expired

--- core/service.js
+++ core/service.js
@@ -3,12 +3,14 @@ var redisNotifier = require('redis-notifier');
 var eventNotifier = new redisNotifier(redis);
 var config = require('../lib/config');
 var cloudinary = require('cloudinary');
+var _ = require('lodash');
 var client = redis.createClient();
 
- module.exports = function(){
+ module.exports.subscribe = function(){
    eventNotifier.on('message', function(pattern, channelPattern, emittedKey){
        var channel = this.parseMessageChannel(channelPattern);
        console.log('here');
+       console.log('key:',channel.key)
        if(channel.key === 'expired'){
            client.hget(config.notification.key, emittedKey, (err,value)=>{
                 if(err){
@@ -21,4 +23,23 @@ var client = redis.createClient();
            });
        }
    })
+}
+
+module.exports.invalidateExplicitly = function(){
+    client.hgetall(config.notification.key,(err,keys)=>{
+        if(err) throw err;
+
+        _.forEach(keys, (key,value)=>{
+            client.hexists(value, "public_id", (err,reply)=>{
+                if(err) throw err;
+                console.log(reply);
+                if(!reply){
+                    console.log(value);
+                    cloudinary.uploader.destroy(value).then(()=>{
+                        client.hdel(config.notification.key, value);
+                    });
+                }
+            })
+        })
+    })
 }
